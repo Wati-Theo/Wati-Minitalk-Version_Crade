604806922a2cef7181c6577d6e6a2e8c11f55638
Wati-Theo42 <schlegel.theo@outlook.fr>
2022-05-13 16:47:53 +0200

    lets goo on touche au bout

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 19:50:46 by tschlege          #+#    #+#             */
-/*   Updated: 2022/05/10 14:31:03 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2022/05/13 16:47:22 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,26 +43,27 @@ void	binary_to_char(void)
 
 void	siga(int bit)
 {
-	msg.current_read_trame_byte++;
-	if (msg.current_read_trame_byte < 31)
-		//add to len
-	else if (msg.current_read_char_byte == 32)
-		//malloc msg.msg
+	if (msg.current_trame_byte < 31)
+		if (bit)
+			msg.msg_len += ft_iterative_power(2, 31 - msg.current_trame_byte);
+	else if (msg.current_char_byte == 31)
+		msg.msg = (sizeof(char) * msg.msg_len + 1)
 	else
-		//lire message, binary to char
+		binary_to_char();
+	msg.current_trame_byte++;
 }
 
 void	on_sigusr1(int sig)
 {
 	siga(0);
 	// printf("0");
-	// msg.current_read_trame_byte++;
-	// if (msg.current_read_trame_byte >= 31)
+	// msg.current_trame_byte++;
+	// if (msg.current_trame_byte >= 31)
 	// {
 	// 	printf(" msg_len: %d\n", msg.msg_len);
 	// 	printf("0");
 	// 	binary_to_char();
-	// 	msg.current_read_char_byte++;
+	// 	msg.current_char_byte++;
 	// }
 }
 
@@ -70,14 +71,14 @@ void	on_sigusr2(int sig)
 {
 	siga(1);
 	// printf("1");
-	// msg.current_read_trame_byte++;
-	// msg.msg_len += ft_iterative_power(2, 31 - msg.current_read_trame_byte);
-	// if (msg.current_read_trame_byte >= 31)
+	// msg.current_trame_byte++;
+	// msg.msg_len += ft_iterative_power(2, 31 - msg.current_trame_byte);
+	// if (msg.current_trame_byte >= 31)
 	// {
 	// 	printf(" msg_len: %d\n", msg.msg_len);
 	// 	printf("0");
 	// 	binary_to_char();
-	// 	msg.current_read_char_byte++;
+	// 	msg.current_char_byte++;
 	// }
 }
 
@@ -85,7 +86,7 @@ int	main(int argc, char *argv[])
 {
 	msg.msg = NULL;
 	msg.msg_len = 0;
-	msg.current_read_trame_byte = 0;
+	msg.current_trame_byte = 0;
 	signal(SIGUSR1, &on_sigusr1);
 	signal(SIGUSR2, &on_sigusr2);
 	printf("LE PID EST %d\n", getpid());
